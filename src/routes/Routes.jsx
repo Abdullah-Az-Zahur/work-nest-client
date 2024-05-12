8f523b954c488c6d4f71d8572f65a21af1eca6ec
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-05-12 16:34:01 +0600

    create update page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,37 +7,45 @@ import JobDetails from "../pages/Home/JobDetails/JobDetails";
 import AddJob from "../pages/AddJob/AddJob";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import MyPostedJobs from "../pages/MyPostedJob/MyPostedJob";
+import UpdateJob from "../pages/UpdateJob/UpdateJob";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    errorElement:<ErrorPage></ErrorPage>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         index: true,
         element: <Home></Home>,
       },
       {
-        path:'/login',
-        element:<Login></Login>
+        path: "/login",
+        element: <Login></Login>,
       },
       {
-        path:'/registration',
-        element:<Registration></Registration>,
+        path: "/registration",
+        element: <Registration></Registration>,
       },
       {
-        path:'/job/:id',
-        element:<JobDetails></JobDetails>,
-        loader: ({params})=> fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`)
+        path: "/job/:id",
+        element: <JobDetails></JobDetails>,
+        loader: ({ params }) =>
+          fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
       },
       {
-        path:'/add-job',
+        path: "/add-job",
         element: <AddJob></AddJob>,
       },
       {
-        path:'/my-posted-jobs',
+        path: "/my-posted-jobs",
         element: <MyPostedJobs></MyPostedJobs>,
+      },
+      {
+        path: "/update/:id",
+        element: <UpdateJob></UpdateJob>,
+        loader: ({ params }) =>
+          fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
       },
     ],
   },
